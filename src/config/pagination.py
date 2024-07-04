39d512cdc9da9280c22b229623008b67e8f0f5b1
Akbarbek Umurzakov <thisisumurzakov@gmail.com>
2024-07-04 23:59:50 +0500

    replace full url with just cursor in pagination

--- src/config/pagination.py
+++ src/config/pagination.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import CursorPagination, PageNumberPagination
+from rest_framework.response import Response
 
 
 class StandardResultsPagination(PageNumberPagination):
@@ -13,3 +14,18 @@ class StandardCursorPagination(CursorPagination):
     max_page_size = 100  # maximum limit for page_size
 
     ordering = "-created_at"  # field to order by
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "next": self.encode_cursor(self.get_next_link()),
+                "previous": self.encode_cursor(self.get_previous_link()),
+                "results": data,
+            }
+        )
+
+    def encode_cursor(self, url):
+        if url is None:
+            return None
+        # Extract the cursor value from the URL
+        return url.split("cursor=")[-1]
