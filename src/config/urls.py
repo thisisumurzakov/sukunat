0c4b6e39338d554534d9dc9296e43d9275231e83
Akbarbek Umurzakov <75569975+thisisumurzakov@users.noreply.github.com>
2024-07-09 14:59:19 +0500

    Revert "kenai initial setup"

--- src/config/urls.py
+++ src/config/urls.py
@@ -43,5 +43,4 @@ urlpatterns = [
     path("v1/sos/", include("sos.urls")),
     path("v1/forum/", include("forum.urls")),
     path("v1/common/", include("common.urls")),
-    path("v1/kenai/", include("kenai.urls")),
 ]
